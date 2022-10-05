7bc4fc84d9760ca006d9f236c6d7078ab0b352d7
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-10-05 20:45:18 +0800

    mongodb added

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -1,49 +1,69 @@
+const Blog = require('../models/blogModel');
 const data = require('../data.json');
 const dogData = data.posts;
 
-//===============================
-exports.getUsers = (req, res) => {
-  res.status(200).json(dogData);
+//==================Get mulitple data=============
+exports.getUsers = async (req, res) => {
+  try {
+    //reading documents from mongodb
+    const blogsData = await Blog.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: blogsData.length,
+      posts: blogsData,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+//===============Get single data===============
+exports.getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.findOne({ id: id });
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+
+  // const user = dogData.find((user) => user.id === id);
 };
 
 //===============================
-exports.getUser = (req, res) => {
-  const id = req.params.id;
-
-  const user = dogData.find((user) => user.id === id);
-
-  return res.status(200).json(user);
+exports.createUser = async (req, res) => {
+  // firstly create all data at once
+  // await Blog.deleteMany({});
+  // const newBlog = await Blog.create(data.posts);
+  // return res.status(200).json({
+  //   status: 'success',
+  //   posts: newBlog,
+  // });
 };
 
 //===============================
-exports.createUser = (req, res) => {
-  return res.status(200).json([
-    ...dogData,
-    {
-      id: '7',
-      name: 'Soul Kitchen Party',
-      slug: 'soul-kitchen-party',
-      venue: 'Onyx Club',
-      address: '60 Gramercy Park N #2 New York, New York(NY), 10010',
-      performers: 'A-Trak, Nightmares on Wax, Rakim, Jay Electronica',
-      date: 'July 2, 2021',
-      time: '8:00 PM EST',
-      description:
-        'Dope party which features Hip Hop legends, emerging artists and world-class turntablists',
-      image: '/images/sample/event7.jpg',
-    },
-  ]);
-};
-
-//===============================
-exports.updateUser = (req, res) => {
-  const id = req.params.id;
-  const user = dogData.find((user) => user.id === id);
-
-  return res.status(200).json({
-    ...user,
-    likes: '50',
-  });
+exports.updateUser = async (req, res) => {
+  const update = req.body;
+  try {
+    const id = req.params.id;
+    const updated = await Blog.findOneAndUpdate(id, update);
+    res.status(200).json({
+      status: 'success',
+      update: updated,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.deleteUser = (req, res) => {
--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection is successful');
+  });
 
 const port = process.env.PORT;
 
