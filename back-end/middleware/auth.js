d90fff8e8e16c445436ef76c7d136645f1e0ee93
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-10-16 20:34:25 +0800

    bugfixed

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,15 +1,19 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-export const auth = async (req, res, next) => {
-  try {
-    const token = req.header('Authorization');
-    if (!token) return res.status(400).json({ msg: 'Login first' });
-    jwt.verify(token, process.env.JWT_SECRET, async (err, res) => {
-      if (err) return res.status(400).json({ msg: 'Login first' });
-      req.user = res;
-      next();
-    });
-  } catch (error) {
-    return res.status(500).json({ msg: error.message });
-  }
+// export const auth = async (req, res, next) => {
+//   try {
+//     const token = req.header('Authorization');
+//     if (!token) return res.status(400).json({ msg: 'Login first' });
+//     jwt.verify(token, process.env.JWT_SECRET, async (err, res) => {
+//       if (err) return res.status(400).json({ msg: 'Login first' });
+//       req.user = res;
+//       next();
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ msg: error.message });
+//   }
+// };
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
 };
